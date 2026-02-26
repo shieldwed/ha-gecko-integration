864a57800fa1e30625b355b057fbdd5feecdc526
Daniel Wilhelm <shieldwed@outlook.com>
2026-02-26 19:34:14 +0100

    feat: add dev tools

---
+++ mise-tasks/print_peps.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# dependencies = ["requests<3", "rich"]
+# ///
+
+import requests
+from rich.pretty import pprint
+
+resp = requests.get("https://peps.python.org/api/peps.json")
+data = resp.json()
+pprint([(k, v["title"]) for k, v in data.items()][:10])
